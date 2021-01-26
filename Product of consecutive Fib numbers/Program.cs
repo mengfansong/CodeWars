8fbf25825648c7d19e10666fb6ccf7c4905bcd52
mengfansong <1034971554@qq.com>
2021-01-26 15:54:04 +0800

    Product of consecutive Fib numbers
    给定一个数，输出相乘后大于等于这个数的最小相邻两个斐波那契数，并输出是等于还是大于。需要注意的是，求斐波那契数的方法不要在循环体中每次调用，会产生过大的计算量。需要把求斐波那契数的方法和循环体有效结合，减少运算时间。

---
+++ Product of consecutive Fib numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Product_of_consecutive_Fib_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(productFib(4895).ToString());
+            ulong[] result = productFib(800);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }        
+
+        public static ulong[] productFib(ulong prod)
+        {                        
+            ulong u = 0;
+            ulong f1 = 0;
+            ulong f2 = 1;
+            ulong temp = 0;
+            ulong[] result = new ulong[3];
+            if (prod == 0)
+            {
+                result[0] = 0;
+                result[1] = 1;
+                result[2] = 0;
+                return result;
+            }
+            while (u<prod)
+            {
+                u = f1*f2;
+                if (u==prod)
+                {
+                    result[0] = f1;
+                    result[1] = f2;
+                    result[2] = 1;
+                    return result;
+                }
+                temp = f1;
+                f1 = f2;
+                f2 = f2 + temp;                
+            }
+            result[0] = temp;
+            result[1] = f1;
+            result[2] = 0;
+            return result;
+        }
+    }
+}
