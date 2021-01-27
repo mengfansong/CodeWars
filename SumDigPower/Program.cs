f3c276d4adf637f261c4f9e8224a53f438c235b0
mengfansong <1034971554@qq.com>
2021-01-27 16:36:26 +0800

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
    找出一个给定范围内的数字数组，这个数组种的数字要满足这样的要求：从前往后，每一位的位置次幂的和等于这个数。比如89 = 8^1 + 9^2。

---
+++ SumDigPower/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Take a Number And Sum Its Digits Raised To The Consecutive Powers And....¡Eureka!!
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Take a Number And Sum Its Digits Raised To The Consecutive Powers And Eureka!!/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Take_a_Number_And_Sum_Its_Digits_Raised_To_The_Consecutive_Powers_And_Eureka__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] l = SumDigPow(1, 140);
+            foreach (var item in l)
+            {
+                Console.WriteLine(item);
+            }          
+        }
+
+
+        public static long[] SumDigPow(long a, long b)
+        {
+            List<long> list = new List<long>();            
+            for (long i = a; i <= b; i++)
+            {
+                string s = i.ToString();
+                double temp = 0;                
+                for (int j = 0; j < s.Length; j++)
+                {
+                    temp += Math.Pow(s[j]-48 ,(j+1));                    
+                }
+                if (i == temp)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
