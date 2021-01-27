79e98a56021e4895616820db643e615ef645997d
mengfansong <1034971554@qq.com>
2021-01-27 15:55:08 +0800

    Vasya - Clerk
    给定一个排队人手持钞票钱数的数组，计算每次收到钱后，手头上的零钱是否足够找零（买票人最开始没有钱），钱的数额为25,50,100。需要注意，给100块钱时，没有50的可以用两张25的代替。

---
+++ Vasya - Clerk/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vasya___Clerk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] people = new int[] { 25, 25, 25,100 };
+            Console.WriteLine(Tickets(people));
+        }
+
+
+        public static string Tickets(int[] peopleInLine)
+        {
+            Dictionary<int, int> purse = new Dictionary<int, int>();
+            purse.Add(25, 0);
+            purse.Add(50, 0);
+            purse.Add(100, 0);
+            if (peopleInLine[0] != 25)
+            {
+                return "NO";
+            }
+            
+            foreach (var money in peopleInLine)
+            {
+                purse[money] += 1;
+                switch (money)
+                {
+                    case 25:
+                        break;
+                    case 50:
+                        purse[25] -= 1;                        
+                        break;
+                    case 100:
+                        if (purse[50] > 0)
+                        {
+                            purse[25] -= 1;
+                            purse[50] -= 1;
+                        }
+                        else if (purse[50] == 0)
+                        {
+                            purse[25] -= 3;
+                        }                        
+                        break;
+                }
+                if (purse[25] < 0)
+                {
+                    return "NO";
+                }
+                if (purse[50] < 0)
+                {
+                    return "NO";
+                }
+            }
+            return "YES";
+        }
+    }
+}
---
+++ 两数之和/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace 两数之和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] int1 = { 2, 7, 11, 15 };
+            int[] ints = TwoSum(int1,9);
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2] ;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j])==target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
