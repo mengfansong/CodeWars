eb497f4e50c484d2f44e3da173d8d81d7b2326e8
mengfansong <1034971554@qq.com>
2021-02-04 13:45:06 +0800

    Where my anagrams at?
    找出字符串数组中与给定字符串组成字母相同的字符串。思路是把字符串中的字母排序组成新字符串，然后比较字符串是否相等，就可以得知组成字母是不是一致了。

---
+++ Where my anagrams at/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Where_my_anagrams_at
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list1 = Anagrams("abcc", new List<string> { "acbc","bbcc","accb" });
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            List<string> list = new List<string>();
+            foreach (var item in words)
+            {
+                if (SortString(word) == SortString(item))
+                {
+                    list.Add(item);
+                }
+            }
+            return list;
+        }
+
+        public static string SortString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            int n = str.Length;
+            bool flag = false;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (chars[j] > chars[j + 1])
+                    {
+                        char temp = chars[j];
+                        chars[j] = chars[j + 1];
+                        chars[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+                if (!flag)
+                {
+                    break;
+                }
+            }           
+            string str1 = new string(chars);            
+            return str1;            
+        }
+
+    }
+}
