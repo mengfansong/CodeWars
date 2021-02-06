9a735af98211a6eff6c192fa32c6b56bd8815edc
mengfansong <1034971554@qq.com>
2021-02-06 22:02:10 +0800

    Count_IP_Addresses
    计算两个IPv4地址之间有多少个地址。

---
+++ Count IP Addresses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Count_IP_Addresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static long IpsBetween(string start, string end)
+        {
+            
+            string[] startStr = start.Split(".");
+            string[] endStr = end.Split(".");
+            long startLong = Convert.ToInt32(startStr[0]) * 256 * 256 * 256 + Convert.ToInt32(startStr[1]) * 256 * 256 +
+                                        Convert.ToInt32(startStr[2]) * 256 + Convert.ToInt32(startStr[3]);
+            long endLong = Convert.ToInt32(endStr[0]) * 256 * 256 * 256 + Convert.ToInt32(endStr[1]) * 256 * 256 +
+                                        Convert.ToInt32(endStr[2]) * 256 + Convert.ToInt32(endStr[3]);
+            return endLong - startLong;
+        }
+    }
+}
