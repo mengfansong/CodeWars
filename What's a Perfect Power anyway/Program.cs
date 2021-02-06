61bef3f144e7465e195b49b5ea82ebe8d3d06712
mengfansong <1034971554@qq.com>
2021-02-06 22:25:45 +0800

    What_s_a_Perfect_Power_anyway
    判断一个数是不是完美幂

---
+++ What's a Perfect Power anyway/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace What_s_a_Perfect_Power_anyway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPerfectPower(81));
+        }
+
+        public static (int, int)? IsPerfectPower(int n)
+        {
+            int p = (int)Math.Sqrt(n);
+            int j = n;
+            for (int i = 2; i <= p; i++)
+            {
+                for (int k = 2; k <= j; k++)
+                {
+
+                    if (Math.Pow(i, k) > n)
+                    {
+                        j = k;
+                        break;
+                    }
+                    if (Math.Pow(i, k) == n)
+                    {
+                        return (i, k);
+                    }
+
+                }
+            }
+            return null;
+        }
+    }
+}
